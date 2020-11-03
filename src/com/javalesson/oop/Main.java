db4bc5ade1c9ba7195662c11e7f7556e856cea44
Alesnim <alesnim@gmail.com>
2020-11-03 17:53:28 +0300

    Update Main.java
    Update file Main.java

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -3,6 +3,6 @@ package com.javalesson.oop;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Example");
+        System.out.println("No Example");
     }
 }
