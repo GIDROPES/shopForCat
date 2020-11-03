db1d7d8165856478aebd42a000211047b6e898fa
alesnim@gmail.com <alesnim@gmail.com>
2020-11-03 18:32:49 +0300

    add something

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Cat Shop");
-
+        JLabel label = new JLabel("LEFT");
+        frame.getContentPane().add(label, "West");
         frame.setSize(600,400 );
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
